33c439a7fbe728426c4e58570250932ead19cb2e
Erwin de Haan <git@erayan.eu>
2012-03-05 22:26:43 +0000

    Uploaded Solution.
    git-svn-id: http://192.168.178.2:1337/svn/erayan@14 12523cd5-8ef4-534a-86f2-1c811af9c56e

---
+++ ExamenStunt/DotNetVersions2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ExamenStunt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Version 2.0 works");
+            Console.ReadKey(true);
+        }
+    }
+}
