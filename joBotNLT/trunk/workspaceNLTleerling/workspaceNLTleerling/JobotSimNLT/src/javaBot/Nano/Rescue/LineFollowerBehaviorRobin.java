b5d393303b3b07b7583fc1fe9274092b8994b40a
Robin Hes <ninjatuna@outlook.com>
2012-03-11 12:31:56 +0000

    26 seconds
    git-svn-id: http://192.168.178.2:1337/svn/erayan@30 12523cd5-8ef4-534a-86f2-1c811af9c56e

--- joBotNLT/trunk/workspaceNLTleerling/workspaceNLTleerling/JobotSimNLT/src/javaBot/Nano/Rescue/LineFollowerBehaviorRobin.java
+++ joBotNLT/trunk/workspaceNLTleerling/workspaceNLTleerling/JobotSimNLT/src/javaBot/Nano/Rescue/LineFollowerBehaviorRobin.java
@@ -2,21 +2,11 @@ package javaBot.Nano.Rescue;
 
 import com.muvium.apt.PeriodicTimer;
 
-/**
- * Opdracht - 6 Eenvoudige Line Follower met enkele sensor
- * 
- * De lijnvolger gebruikt de reflectiesensoren (fieldsensoren)
- * om de kleur van het veld onder de robot te bepalen.
- * Hij volgt de zwarte lijn, eerst met een enkele sensor.
- * In opdracht 7 ga je de lijn met twee sensoren volgen.
- * Daarnaast ga je ook een subroutine gebruiken.
- * Als laatste ga je ook de gele lijn volgen.
- */
-
 public class LineFollowerBehaviorRobin extends Behavior
 {
 	private BaseController	joBot;
 	private int	state		= 0;
+	private int prevstate	= 0;
 	private String dir		= "none";
 	private String prevdir 	= "none";
 	private int speedL		= 0;
@@ -25,6 +15,9 @@ public class LineFollowerBehaviorRobin extends Behavior
 	private int sensorFR 	= 0;
 	private int valueBlack	= 400;   // Value of black on your field
 	private int valueYellow = 700; // Value of yellow on your field
+	private long startTime	= 0;
+	private long finishTime	= 0;
+	private long stopTime	= 0;
 	
 	public LineFollowerBehaviorRobin(BaseController initJoBot, PeriodicTimer
 			initServiceTick,int servicePeriod)
@@ -38,33 +31,50 @@ public class LineFollowerBehaviorRobin extends Behavior
 	
 	public void doBehavior() {
 		if (state == 0) {
+			
+			if(prevstate != state) {
+				joBot.printLCD("Idle");
+			}
+			
 			System.out.println("Line Follower by Robin");
 			joBot.setStatusLeds(false, false, false);
 			joBot.setFieldLeds(true);	// Zet field leds aan
 			state = 1;
+			
+			
 		}
 
-		if (state == 1) {	
+		//Driving on black lines
+		if (state == 1) {
+			
+			if(prevstate != state) {
+				joBot.printLCD("Black line");
+			}
+			
+			if(prevstate == 0) {
+				startTime = System.nanoTime();
+			}
+			
 			sensorFL = joBot.getSensorValue(BaseController.SENSOR_FL); // Links sensor
 			sensorFR = joBot.getSensorValue(BaseController.SENSOR_FR); // Rechts sensor		
 
 			if (sensorFL >= valueBlack && sensorFR < valueBlack) {
-				speedL = 40;
-				speedR = 10;
+				speedL = 80;
+				speedR = 20;
 				joBot.setLed(BaseController.LED_GREEN, true);
 				dir = "Right";
 			}
 				
 			if (sensorFR >= valueBlack && sensorFL < valueBlack) {
-				speedL = 10;
-				speedR = 40;
+				speedL = 20;
+				speedR = 80;
 				joBot.setLed(BaseController.LED_GREEN, true);
 				dir = "Left";
 			}
 			
 			if(sensorFL < valueBlack && sensorFR < valueBlack) {
-				speedL = 50;
-				speedR = 50;
+				speedL = 70;
+				speedR = 70;
 				joBot.setLed(BaseController.LED_GREEN, false);
 				joBot.setLed(BaseController.LED_RED, true);
 				dir = "Forward";
@@ -73,13 +83,13 @@ public class LineFollowerBehaviorRobin extends Behavior
 			if(sensorFR > valueBlack && sensorFL > valueBlack) {
 				
 				if(prevdir == "Right") {
-					speedL = 30;
-					speedR = -30;
+					speedL = 40;
+					speedR = -40;
 				}
 				
 				if (prevdir == "Left") {
-					speedL = -30;
-					speedR = 30;
+					speedL = -40;
+					speedR = 40;
 				}
 				
 			}
@@ -89,21 +99,26 @@ public class LineFollowerBehaviorRobin extends Behavior
 			}
 		}
 		
+		//Driving on yellow lines
 		if(state == 2) {
+			
+			if(prevstate != state) {
+				joBot.printLCD("Yellow line");
+			}
 			
 			sensorFL = joBot.getSensorValue(BaseController.SENSOR_FL); // Links sensor
 			sensorFR = joBot.getSensorValue(BaseController.SENSOR_FR); // Rechts sensor		
 
 			if (sensorFL <= valueYellow && sensorFR > valueYellow) {
-				speedL = 40;
-				speedR = 10;
+				speedL = 80;
+				speedR = 20;
 				joBot.setLed(BaseController.LED_GREEN, true);
 				dir = "Right";
 			}
 				
 			if (sensorFR < valueYellow && sensorFL >= valueYellow) {
-				speedL = 10;
-				speedR = 40;
+				speedL = 20;
+				speedR = 80;
 				joBot.setLed(BaseController.LED_GREEN, true);
 				dir = "Left";
 			}
@@ -117,43 +132,36 @@ public class LineFollowerBehaviorRobin extends Behavior
 				
 			}
 			
-			if(sensorFR < valueYellow && sensorFL < valueYellow) {
-				
-				if(prevdir == "Right") {
-					speedL = 30;
-					speedR = -30;
-				}
-				
-				if (prevdir == "Left") {
-					speedL = -30;
-					speedR = 30;
-				}
-				
-			}
-			
 			if(sensorFR <= valueBlack || sensorFL >= valueBlack) {
 				state = 1;
 			}
 			
-			if(sensorFR == 1000 && sensorFL == 1000) {
+			if(sensorFR >= 1000 && sensorFL >= 1000) {
 				state = 3;
 			}
 				
 		}
-		
+			
+		//Stopping when arriving at the yellow plain
 		if(state == 3) {
+			
+			if(prevstate != state) {
+				joBot.printLCD("Stop");
+				stopTime = System.nanoTime();
+				finishTime = (stopTime - startTime)/1000000000;
+				System.out.print("Finished in " + finishTime + " seconds.");
+			}
+			
 			speedL = 0;
 			speedR = 0;
 		}
 		
 		//drive this shit
-		joBot.drive(speedL, speedR);
+		joBot.drive(speedL, speedR);	
 		
-		System.out.println(state);
-		
-		
-		//remember previous direction
+		//remember previous direction and state
 		prevdir = dir;
+		prevstate = state;
 		
 		
 	}
