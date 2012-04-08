0eb5c653c1f578ac2d2465d0ba3ddf5622d7b2dc
Erwin de Haan <git@erayan.eu>
2012-04-08 15:51:56 +0000

    git-svn-id: http://192.168.178.2:1337/svn/erayan@48 12523cd5-8ef4-534a-86f2-1c811af9c56e

---
+++ emp/EraYaN's Metadata Processor/exceptionHandling.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace EMP
+{
+    class exceptionHandling
+    {
+        public enum exceptionLevel
+        {
+            EMP_FATAL_ERROR = 0,
+            EMP_ERROR = 1,
+            EMP_WARNING = 2,
+            EMP_NOTICE = 3            
+        }
+        static public void triggerExeption(String message, exceptionLevel eLevel = exceptionLevel.EMP_NOTICE, Exception exception = null)
+        {
+            MessageBox.Show(message+"\r\n"+eLevel.ToString());
+        }
+    }
+}
