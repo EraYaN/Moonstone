68b19138139131c9e0c2a7df1a3b2633842f47ca
Erwin de Haan <git@erayan.eu>
2012-04-08 12:22:47 +0000

    blah
    git-svn-id: http://192.168.178.2:1337/svn/erayan@42 12523cd5-8ef4-534a-86f2-1c811af9c56e

--- emp/EraYaNs Metadata Processor/MainWindow.xaml.cs
+++ emp/EraYaNs Metadata Processor/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace EraYaNs_Metadata_Processor
+namespace EMP
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
