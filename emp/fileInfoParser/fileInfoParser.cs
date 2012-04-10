6b480235a9cbb891c18ad9a2dfbbed3b47dd5d85
Robin Hes <ninjatuna@outlook.com>
2012-04-10 16:24:55 +0000

    git-svn-id: http://192.168.178.2:1337/svn/erayan@53 12523cd5-8ef4-534a-86f2-1c811af9c56e

--- emp/fileInfoParser/fileInfoParser.cs
+++ emp/fileInfoParser/fileInfoParser.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,10 +8,35 @@ namespace EMP
 {
     class fileInfoParser
     {
+        public string debugString;
+        public string fileName;
+        public DirectoryInfo fileDir;
+        public string fileDirName;
 
+        public fileInfoParser(FileInfo fileInfo)
+        {
+            //Some obvious shit first
+            fileName = fileInfo.Name;
+            fileDir = fileInfo.Directory;
+            fileDirName = fileDir.Name;
 
+            parse(fileName);
+            parse(fileDirName);
 
-        string debugString;
+            debugString = "Processed file:\t\t" + fileName +
+                "\nIn directory:\t\t" + fileDirName;
+
+        }
+
+        private void parse(string inputString)
+        {
+            string[] split = inputString.Split(new Char[] { ' ', '.', '-', '_' });
+
+            for (int i = 0; i < split.Count(); i++)
+            {
+                string s = split[i];
+            }
+        }
 
         //Used for printing useful info stored in "debugString", can be called in another class by calling this constructor
         public override string ToString()
