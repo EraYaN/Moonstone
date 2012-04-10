5d7ee9d86756350b33640520e4bafd30d2945126
Robin Hes <ninjatuna@outlook.com>
2012-04-10 14:31:34 +0000

    git-svn-id: http://192.168.178.2:1337/svn/erayan@52 12523cd5-8ef4-534a-86f2-1c811af9c56e

---
+++ emp/fileInfoParser/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EMP
+{
+    class Program
+    {
+        static void Main()
+        {
+            fileInfoParser c = new fileInfoParser();
+
+            Console.WriteLine(c);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ emp/fileInfoParser/fileInfoParser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EMP
+{
+    class fileInfoParser
+    {
+
+
+
+        string debugString;
+
+        //Used for printing useful info stored in "debugString", can be called in another class by calling this constructor
+        public override string ToString()
+        {
+            return "Debugging info:\n\n" + debugString + "\n";
+        }
+    }
+}
