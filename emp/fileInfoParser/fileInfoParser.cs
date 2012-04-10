d66f1cc9f24c451ee9d66ff84b77f840e2469745
Robin Hes <ninjatuna@outlook.com>
2012-04-10 21:08:43 +0000

    git-svn-id: http://192.168.178.2:1337/svn/erayan@55 12523cd5-8ef4-534a-86f2-1c811af9c56e

--- emp/fileInfoParser/fileInfoParser.cs
+++ emp/fileInfoParser/fileInfoParser.cs
@@ -3,13 +3,16 @@ using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace EMP
 {
     class fileInfoParser
     {
         public string debugString;
+
         public string fileName;
+
         public DirectoryInfo fileDir;
         public string fileDirName;
 
@@ -17,6 +20,8 @@ namespace EMP
         public string year = "Unknown";
         public string quality = "Unknown";
         public string source = "Unknown";
+        public string filetype = "Unknown";
+        public string encoding = "Unknown";
 
         public fileInfoParser(FileInfo fileInfo)
         {
@@ -34,7 +39,9 @@ namespace EMP
                 "\n\nProperties:" + 
                 "\nTitle:\t\t\t" + title +
                 "\nQuality:\t\t" + quality +
-                "\nSource:\t\t\t" + source;
+                "\nSource:\t\t\t" + source +
+                "\nFiletype:\t\t" + filetype +
+                "\nEncoding:\t\t" + encoding;
 
         }
 
@@ -51,11 +58,20 @@ namespace EMP
 
             string[] sources = new string[3] { "brrip", "bluray", "dvdrip" };
             string[] sourceNames = new string[3] { "Blu-ray Rip", "Blu-ray Rip", "DVD Rip" };
+
+            string[] filetypes = new string[3] { "mkv", "avi", "mp4" };
+            string[] filetypeNames = new string[3] { "Matroschka Video (.mkv)", "Microsoft AVI (.avi)", "MPEG-4 (.mp4)" };
+
+            string[] encodings = new string[4] { "x264", "h264", "xvid", "divx" };
+            string[] encodingNames = new string[4] { "x264 Encoding", "H.264 Encoding", "Xvid Encoding", "DivX Encoding" };
             
             //This array is used to keep track of the location (index) at which certain info can be found
             //It will also be used to determine where the actual title of the movie ends and where the other crap like releasegroups starts
             //It is important to keep the indexes in the correct order! By default the indexes are in the same order as the above arrays, ending with the year index
             int[] indices = new int[5];
+
+            //A little regex for recognizing the year
+            Regex rgx = new Regex(@"\W(\d{4})\W");
 
             for (int i = 0; i < split.Count(); i++)
             {
@@ -66,13 +82,32 @@ namespace EMP
                     indices[0] = i;
                     i = -1;
                 }
-                if (Array.IndexOf(sources, s.ToLower()) != -1 & indices[1] < 1)
+                else if (Array.IndexOf(sources, s.ToLower()) != -1 & indices[1] < 1)
                 {
                     source = sourceNames[Array.IndexOf(sources, s.ToLower())];
                     indices[1] = i;
                     i = -1;
                 }
-
+                else if (Array.IndexOf(filetypes, s.ToLower()) != -1 & indices[2] < 1)
+                {
+                    filetype = filetypeNames[Array.IndexOf(filetypes, s.ToLower())];
+                    indices[2] = i;
+                    i = -1;
+                }
+                else if (Array.IndexOf(encodings, s.ToLower()) != -1 & indices[3] < 1)
+                {
+                    encoding = encodingNames[Array.IndexOf(encodings, s.ToLower())];
+                    indices[3] = i;
+                    i = -1;
+                }
+                else if (rgx.IsMatch(s))
+                {
+                    for (int i2 = 0; i2 < rgx.Matches(inputString).Count;i2++)
+                    {
+                        Match match = rgx.Matches(inputString)[i2];
+                        Console.WriteLine(match);
+                    }
+                }
             }
         }
 
