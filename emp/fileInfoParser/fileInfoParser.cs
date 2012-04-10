882fd23c61fd09dec86145a7ddebe4d1c34869cb
Robin Hes <ninjatuna@outlook.com>
2012-04-10 18:15:19 +0000

    git-svn-id: http://192.168.178.2:1337/svn/erayan@54 12523cd5-8ef4-534a-86f2-1c811af9c56e

--- emp/fileInfoParser/fileInfoParser.cs
+++ emp/fileInfoParser/fileInfoParser.cs
@@ -13,6 +13,11 @@ namespace EMP
         public DirectoryInfo fileDir;
         public string fileDirName;
 
+        public string title = "Unknown";
+        public string year = "Unknown";
+        public string quality = "Unknown";
+        public string source = "Unknown";
+
         public fileInfoParser(FileInfo fileInfo)
         {
             //Some obvious shit first
@@ -23,18 +28,51 @@ namespace EMP
             parse(fileName);
             parse(fileDirName);
 
-            debugString = "Processed file:\t\t" + fileName +
-                "\nIn directory:\t\t" + fileDirName;
+            debugString = "General:\n" + 
+                "\nProcessed file:\t\t" + fileName +
+                "\nIn directory:\t\t" + fileDirName + 
+                "\n\nProperties:" + 
+                "\nTitle:\t\t\t" + title +
+                "\nQuality:\t\t" + quality +
+                "\nSource:\t\t\t" + source;
 
         }
 
         private void parse(string inputString)
         {
+            //First we split the string into an array of smaller, easier to handle substrings
             string[] split = inputString.Split(new Char[] { ' ', '.', '-', '_' });
+
+            //Here we specify a couple of string arrays which could match certain substrings
+            //If there is a match, it will give us some more info about the file's properties
+            //For display, we use another array containing the pretty names for each property
+            string[] qualities = new string[2] { "720p", "1080p" };
+            string[] qualityNames = new string[2] { "720p HD", "1080p HD" };
+
+            string[] sources = new string[3] { "brrip", "bluray", "dvdrip" };
+            string[] sourceNames = new string[3] { "Blu-ray Rip", "Blu-ray Rip", "DVD Rip" };
+            
+            //This array is used to keep track of the location (index) at which certain info can be found
+            //It will also be used to determine where the actual title of the movie ends and where the other crap like releasegroups starts
+            //It is important to keep the indexes in the correct order! By default the indexes are in the same order as the above arrays, ending with the year index
+            int[] indices = new int[5];
 
             for (int i = 0; i < split.Count(); i++)
             {
                 string s = split[i];
+                if (Array.IndexOf(qualities, s.ToLower()) != -1 & indices[0] < 1)
+                {
+                    quality = qualityNames[Array.IndexOf(qualities, s.ToLower())];
+                    indices[0] = i;
+                    i = -1;
+                }
+                if (Array.IndexOf(sources, s.ToLower()) != -1 & indices[1] < 1)
+                {
+                    source = sourceNames[Array.IndexOf(sources, s.ToLower())];
+                    indices[1] = i;
+                    i = -1;
+                }
+
             }
         }
 
