20e8c7b33b15552414f5e017f6a670def6263ca3
Erwin de Haan <git@erayan.eu>
2012-04-11 12:52:21 +0000

    Added docs
    git-svn-id: http://192.168.178.2:1337/svn/erayan@59 12523cd5-8ef4-534a-86f2-1c811af9c56e

--- emp/exceptionHandler/exceptionHandler.cs
+++ emp/exceptionHandler/exceptionHandler.cs
@@ -15,6 +15,18 @@ namespace EMP
             EMP_WARNING = 2,
             EMP_NOTICE = 3            
         }
+        /// <summary>
+        ///    Handles exceptions
+        /// </summary>
+        /// <param name="message">
+        ///    The error message to display to the user or add to the log.
+        /// </param>
+        /// <param name="eLevel">
+        ///    The severity of the error, a <see cref="exeptionLevel"/> value. Deafult is <see cref="exceptionLevel.EMP_NOTICE"/>.
+        /// </param>
+        /// <param name="exception">
+        ///    The <see cref="Exception"/> object returned by the runtime or usercode.
+        /// </param>
         static public void triggerExeption(String message, exceptionLevel eLevel = exceptionLevel.EMP_NOTICE, Exception exception = null)
         {
             MessageBox.Show(message+"\r\n"+eLevel.ToString());
