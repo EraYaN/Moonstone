3286b1503c53d1cfe8c8b96f5c1608f498c0b012
Erwin de Haan <git@erayan.eu>
2012-04-17 20:08:48 +0000

    Added ProgressBar, StatusBar. Put scanning in backgroundworker.
    git-svn-id: http://192.168.178.2:1337/svn/erayan@76 12523cd5-8ef4-534a-86f2-1c811af9c56e

--- emp/EraYaN's Metadata Processor/MainWindow.xaml.cs
+++ emp/EraYaN's Metadata Processor/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using TagLib;
 using System.IO;
+using System.ComponentModel;
 
 namespace EMP
 {
@@ -22,12 +23,87 @@ namespace EMP
     /// </summary>
     public partial class MainWindow : Window
     {
+        public BackgroundWorker bw = new BackgroundWorker();
         public MainWindow()
         {
             InitializeComponent();
+            bw.WorkerReportsProgress = true;
+            bw.WorkerSupportsCancellation = true;
+            bw.DoWork += new DoWorkEventHandler(bw_DoWork);
+            bw.ProgressChanged += new ProgressChangedEventHandler(bw_ProgressChanged);
+            bw.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bw_RunWorkerCompleted);
+        }
+
+        void bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            textBlockStatus.Text = "Completed";
+            progressBarScan.Value = 100;
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            textBlockData.Text = "Data (MB):\n" + Math.Round((double)GC.GetTotalMemory(true) / 1024 / 1024, 2);
+        }
+
+        void bw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBarScan.Value = e.ProgressPercentage;
+            if (e.ProgressPercentage > 35)
+            {
+                int hello = 10;
+            }
+            textBlockStatus.Text = "Scanning...";
+            writeLine((String)e.UserState);
+            textBlockData.Text = "Data (MB):\n" + Math.Round((double)GC.GetTotalMemory(false) / 1024 / 1024, 2);
+        }
+
+        void bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            DirectoryInfo dirinfo = (DirectoryInfo)e.Argument;
+            FileInfo[] files = dirinfo.GetFiles("*.m??", SearchOption.AllDirectories);
+            double count = files.Count();
+            double filenum = 0;
+            bw.ReportProgress((int)Math.Round(filenum/count*100),"Count: "+count);
+            //Timer
+            Stopwatch swProcessTime = new Stopwatch();
+            swProcessTime.Start();
+            foreach (FileInfo file in files)
+            {
+                filenum++;
+                try
+                {
+                                        
+                    bw.ReportProgress((int)Math.Round(filenum / count * 100), "\r\n"+file.Name);
+                    TagLib.File fileTag = TagLib.File.Create(file.FullName);
+                    //Timer
+                    Stopwatch swFileTime = new Stopwatch();
+                    swFileTime.Start();
+                    //Parse results
+                    fileInfoParser fileInfoParser = new fileInfoParser(file);
+                    bw.ReportProgress((int)Math.Round(filenum / count * 100), "Parse result: " + fileInfoParser);
+                    bw.ReportProgress((int)Math.Round(filenum / count * 100), "TagType: " + fileTag.TagTypes.ToString());
+                    bw.ReportProgress((int)Math.Round(filenum / count * 100), "Title: " + fileTag.Tag.Title + "; Year: " + fileTag.Tag.Year);
+                    //Timer output
+                    swFileTime.Stop();
+                    TimeSpan fileTime = swFileTime.Elapsed;
+                    bw.ReportProgress((int)Math.Round(filenum / count * 100), "Parsed in " + fileTime.TotalMilliseconds + "ms");
+                    fileInfoParser = null;
+                    fileTag.Dispose();
+                    fileTag = null;
+                }
+                catch (Exception exception)
+                {
+                    bw.ReportProgress((int)Math.Round(filenum / count * 100), "ERROR processing file.");
+                    exceptionHandler.triggerException(exception.Message);
+                }
+            }
+
+            swProcessTime.Stop();
+            TimeSpan processTime = swProcessTime.Elapsed;
+            bw.ReportProgress(100, "All files (" + files.Count() + ") parsed in " + processTime.TotalMilliseconds + " ms");
+            files = null;
+            dirinfo = null;
         }       
 
-        private void button1_Click(object sender, RoutedEventArgs e)
+        private void buttonGo_Click(object sender, RoutedEventArgs e)
         {
             writeLine();
             /*foreach (String str in SupportedMimeType.AllMimeTypes)
@@ -39,43 +115,28 @@ namespace EMP
             {
                 dirinfo = new DirectoryInfo(@"\\SERVER\Users\Admin\Videos\Movies");
             }
-            FileInfo[] files = dirinfo.GetFiles("*.m??",SearchOption.AllDirectories);
-            writeLine("Count: "+files.Count());
-            //Timer
-            Stopwatch swProcessTime = new Stopwatch();
-            swProcessTime.Start();
-            foreach(FileInfo file in files){
-                try{
-                    writeLine();
-                    writeLine(file.Name);
-                    TagLib.File fileTag = TagLib.File.Create(file.FullName);
-                    //Timer
-                    Stopwatch swFileTime = new Stopwatch();
-                    swFileTime.Start();
-                    //Parse results
-                    fileInfoParser fileInfoParser = new fileInfoParser(file);
-                    writeLine("Parse result: " + fileInfoParser);
-                    writeLine("TagType: " + fileTag.TagTypes.ToString());
-                    writeLine("Title: " + fileTag.Tag.Title + "; Year: " + fileTag.Tag.Year);
-                    //Timer output
-                    swFileTime.Stop();
-                    TimeSpan fileTime = swFileTime.Elapsed;
-                    writeLine("Parsed in " + fileTime.TotalMilliseconds + "ms");
-                } catch(Exception exception){
-                    writeLine("ERROR processing file.");
-                    exceptionHandler.triggerException(exception.Message);                    
-                }
-            }
-            swProcessTime.Stop();
-            TimeSpan processTime = swProcessTime.Elapsed;
-            writeLine("All files (" + files.Count() + ") parsed in " + processTime.TotalMilliseconds + " ms");
-            
+            //bw worker
+            bw.RunWorkerAsync(dirinfo);
+                        
         }
         public void writeLine(String line){
             textBoxTagLibTest.Text += line+"\r\n";
         }
         public void writeLine(){
             textBoxTagLibTest.Text += "\r\n";
+        }
+
+        private void buttonClear_Click(object sender, RoutedEventArgs e)
+        {
+            textBoxTagLibTest.Text = "";
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            textBlockData.Text = "Data (MB):\n" + Math.Round((double)GC.GetTotalMemory(true) / 1024 / 1024, 2);
+        }
+
+        private void mainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            textBlockData.Text = "Data (MB):\n" + Math.Round((double)GC.GetTotalMemory(true) / 1024 / 1024, 2);
         }
     }
 }
