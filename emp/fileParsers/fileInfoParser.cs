bd46d40ed822360cf636bc1fe3685839d6e61d83
Robin Hes <ninjatuna@outlook.com>
2012-04-18 20:41:02 +0000

    git-svn-id: http://192.168.178.2:1337/svn/erayan@79 12523cd5-8ef4-534a-86f2-1c811af9c56e

--- emp/fileParsers/fileInfoParser.cs
+++ emp/fileParsers/fileInfoParser.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Windows;
 
 namespace EMP
 {
@@ -25,6 +26,10 @@ namespace EMP
         public string codec = "Unknown";
         public string audioCodec = "Unknown";
         public string other = "None";
+
+        int[] indices = new int[6];
+        string[] processed = new string[20];
+        int iP = 0; //index for processed array
 
         /// <summary>
         /// Constructor for movie and/or show data.
@@ -61,9 +66,6 @@ namespace EMP
         /// <param name="inputString">The string to process.</param>
         private void parse(string inputString)
         {
-            //First we split the string into an array of smaller, easier to handle substrings
-            string[] split = inputString.Split(new Char[] { ' ', '.', '_' });
-
             //Here we specify a couple of string arrays which could match certain substrings
             //If there is a match, it will give us some more info about the file's properties
             //For display, we use another array containing the pretty names for each property
@@ -82,143 +84,89 @@ namespace EMP
             string[] audioCodecs = new string[4] { "dts", "aac", "ac3", "mp3" };
             string[] audioCodecNames = new string[4] { "DTS Audio Codec (DTS)", "Advanced Audio Coding (AAC)", "Dolby Digital (AC3)", "MPEG-2 Audio Layer III (MP3)" };
 
-            //This array is used to keep track of the location (index) at which certain info can be found
-            //It will also be used to determine where the actual title of the movie ends and where the other crap like releasegroups starts
-            //It is important to keep the indexes in the correct order! By default the indexes are in the same order as the above arrays, with the "year" at index 4
-            int[] indices = new int[6];
-
             //A little regex for recognizing the year
             Regex rgx = new Regex(@"\b((19|20)\d{2})\b");
 
-            //Array to store possible parts of the title with an extra integer to store the current index in the array
-            string[] titleArray = new string[20];
-            int iTitle = 0;
+            //Processing dat shit
+            //First checking for preset properties
+            quality = check(inputString, qualities, qualityNames);
+            source = check(inputString, sources, sourceNames);
+            filetype = check(inputString, filetypes, filetypeNames);
+            codec = check(inputString, codecs, codecNames);
+            audioCodec = check(inputString, audioCodecs, audioCodecNames);
 
-            string[] otherArray = new string[5];
-            int iOther = 0;
-            int iOtherRev = otherArray.Count() - 1; //Reverse index counter for storing values at the end
-
-            //Processing dat shit.
-            for (int i = 0; i < qualities.Count(); i++)
-            {
-                string c = qualities[i];
-
-                if (inputString.ToLower().Contains(c))
-                {
-                    quality = qualityNames[i];
-                    indices[0] = inputString.IndexOf(c);
-                }
-            }
-
-            for (int i = 0; i < sources.Count(); i++)
-            {
-                string c = sources[i];
-
-                if (inputString.ToLower().Contains(c))
-                {
-                    source = sourceNames[i];
-                    indices[1] = inputString.IndexOf(c);
-                }
-            }
-
-            for (int i = 0; i < filetypes.Count(); i++)
-            {
-                string c = filetypes[i];
-
-                if (inputString.ToLower().Contains(c))
-                {
-                    filetype = filetypeNames[i];
-                    indices[2] = inputString.IndexOf(c);
-
-                }
-            }
-
-            for (int i = 0; i < codecs.Count(); i++)
-            {
-                string c = codecs[i];
-
-                if (inputString.ToLower().Contains(c))
-                {
-                    codec = codecNames[i];
-                    indices[3] = inputString.IndexOf(c);
-                }
-            }
-
-            for (int i = 0; i < audioCodecs.Count(); i++)
-            {
-                string c = audioCodecs[i];
-
-                if (inputString.ToLower().Contains(c))
-                {
-                    audioCodec = audioCodecNames[i];
-                    indices[5] = inputString.IndexOf(c);
-                }
-            }
-
+            //Regex for the year
             if (rgx.IsMatch(inputString))
             {
                 year = rgx.Matches(inputString)[rgx.Matches(inputString).Count - 1].ToString();
                 indices[4] = inputString.IndexOf(year);
+                processed[iP] = year;
+                iP++;
             }
 
-            //The string is now processed, although we do not have a title yet
-            //Following code is to remove all shit from the "titleArray" and, in normal circumstances, only keep the title itself
-            //To determine where the title ends, we check where the other info starts
-            //When this is done, we can use the index whereat the other info starts as a separator to cut off any leftover strings apart from the title
+            //Determine where the title ends
+            int[] indicesSorted = indices; //Copy it
+            Array.Sort(indicesSorted); //Sort it, adress 0 now contains the lowest value
+            int separation = 0;
 
-            //If no title could be found, we will assume the title is actually represented by a year (ie the movie "2012", which was released in 2009)
-            if (titleArray[0] == null)
+            for (int i = 0; i < indicesSorted.Count(); i++)
             {
-                title = year;
-                year = "Unknown";
-            }
-            else
-            {
-                int[] indicesSorted = indices; //Copy it
-                Array.Sort(indicesSorted); //Sort it, adress 0 now contains the lowest value
-                int separation = 0;
-
-                for (int i = 0; i < indicesSorted.Count(); i++)
+                int n = indicesSorted[i];
+                if (n > 0)
                 {
-                    int n = indicesSorted[i];
-                    if (n > 0)
-                    {
-                        separation = indicesSorted[i];
-                        break;
-                    }
-                }
-
-                for (int i = 0; i < titleArray.Count(); i++)
-                {
-                    string s = titleArray[i];
-                    if (Array.IndexOf(split, s) > separation)
-                    {
-                        if (iOtherRev != otherArray.Count())
-                        {
-                            otherArray[iOther] = s + " ";
-                        }
-                        else
-                        {
-                            if (iOther == 0)
-                            {
-                                otherArray[iOther] = s;
-                            }
-                            else
-                            {
-                                otherArray[iOther] = " " + s;
-                            }
-                        }
-                        iOther++;
-                        titleArray[i] = "";
-                    }
-                    else if (i > 0)
-                    {
-                        titleArray[i] = " " + s;
-                    }
+                    separation = indicesSorted[i];
+                    break;
                 }
             }
-            title = string.Concat(titleArray);
-            other = string.Concat(otherArray);
+
+            //Getting the title itself
+            title = inputString.Substring(0, separation - 1);
+            processed[iP] = title;
+            iP++;
+
+            char[] spaceChars = new char[]{'.','_'};
+            foreach (char c in spaceChars)
+            {
+                title = title.Replace(c, ' ');
+            }
+
+            //Extract any leftover shit into "other"
+            other = inputString;
+            foreach (string p in processed)
+            {
+                if (p != null)
+                {
+                   // MessageBox.Show(title + p);
+                    other = other.Replace(p, null);
+                }
+
+            }
+        }
+
+        /// <summary>
+        /// Checks for a preset string in the provided string and returns its "pretty name" (prop)
+        /// </summary>
+        /// <param name="input">The string to check</param>
+        /// <param name="props">An array with preset properties to find in "input"</param>
+        /// <param name="propNames">An array with "pretty names", from whitch a match will be returned as "prop"</param>
+        /// <returns>prop</returns>
+        private string check(string input, string[] props, string[] propNames)
+        {
+            string prop = "Unknown";
+                for (int i = 0; i < props.Count(); i++)
+                {
+                    string c = props[i];
+
+                    if (input.ToLower().Contains(c))
+                    {
+                        prop = propNames[i];
+                        indices[iP] = input.IndexOf(c);
+                        //MessageBox.Show(c.Length.ToString());
+                        processed[iP] = input.Substring(input.ToLower().IndexOf(c), c.Length);
+                        iP++;
+                    }
+                }
+            return prop;
         }
 
         //Used for printing useful info stored in "debugString", can be called in another class by calling this constructor
