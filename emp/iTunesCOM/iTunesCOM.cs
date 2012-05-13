92f6685c87142f083a81276d308eb7b589710f21
Erwin de Haan <git@erayan.eu>
2012-05-13 18:32:16 +0000

    Moved iTunesCOM class to external project.
    git-svn-id: http://192.168.178.2:1337/svn/erayan@90 12523cd5-8ef4-534a-86f2-1c811af9c56e

---
+++ emp/iTunesCOM/iTunesCOM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using iTunesLib;
+
+namespace EMP
+{
+    class iTunesCOM
+    {
+        iTunesApp app = new iTunesApp();
+        public iTunesCOM()
+        { 
+
+        }
+        public string GetState()
+        {
+            ITPlayerState playerState = app.PlayerState;
+            return playerState.ToString();
+        }        
+    }
+}
