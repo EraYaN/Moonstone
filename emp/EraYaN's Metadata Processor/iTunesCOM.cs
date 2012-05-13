71dd275e88a526bdd87e16db1a48d2dc18d25794
Erwin de Haan <git@erayan.eu>
2012-05-13 17:44:19 +0000

    git-svn-id: http://192.168.178.2:1337/svn/erayan@89 12523cd5-8ef4-534a-86f2-1c811af9c56e

--- emp/EraYaN's Metadata Processor/iTunesCOM.cs
+++ emp/EraYaN's Metadata Processor/iTunesCOM.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using iTunesLib;
 
 namespace EMP
 {
     class iTunesCOM
     {
+        iTunesApp app = new iTunesApp();
+        public iTunesCOM()
+        {
+                     
+
+        }
+        public ~iTunesCOM()
+        {
+        
+        }
     }
 }
