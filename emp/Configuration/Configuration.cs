8fc38590f88f16d53813598188c9904049e1f89b
Erwin de Haan <git@erayan.eu>
2012-05-14 22:45:49 +0000

    git-svn-id: http://192.168.178.2:1337/svn/erayan@108 12523cd5-8ef4-534a-86f2-1c811af9c56e

--- emp/Configuration/Configuration.cs
+++ emp/Configuration/Configuration.cs
@@ -2,11 +2,51 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Runtime.Serialization;
 
 namespace EMP
 {
-    public class Configuration
+    [Serializable()]
+    public class Configuration : ISerializable 
     {
-        Tab[] tabs;
+        public List<Entities.Tab> Tabs;
+        public Configuration()
+        {
+            Tabs = new List<Entities.Tab>();
+            //add config with default values
+            Entities.Setting setting = new Entities.Setting();
+            setting.Name = "_Test Blah";//accelerator key is T
+            setting.Identifier = "testBlah";
+            setting.Value = "Test Value";
+            setting.Type = typeof(String);
+            Entities.Group group = new Entities.Group();
+            group.Settings = new List<Entities.Setting>();
+            group.Name = "Other";
+            group.Identifier = "other";
+            group.Settings.Add(setting);
+            Entities.Tab tab = new Entities.Tab();
+            tab.Identifier = "general";
+            tab.Name = "General";
+            tab.Groups = new List<Entities.Group>();
+            tab.Groups.Add(group);
+            Tabs.Add(tab);
+        }
+        public Configuration(String ConfigPath)
+        {
+            //add config with default values and save to configpath
+        }
+        protected Configuration(SerializationInfo info, StreamingContext context)
+        {
+            if (info == null)
+                throw new System.ArgumentNullException("info");
+            Tabs = (List<Entities.Tab>)info.GetValue("Tabs", typeof(List<Entities.Tab>));            
+        }
+        public virtual void GetObjectData(
+        SerializationInfo info, StreamingContext context)
+        {
+            if (info == null)
+                throw new System.ArgumentNullException("info");
+            info.AddValue("Tabs", Tabs);           
+        }
     }
 }
---
+++ emp/Configuration/Entities.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EMP
+{
+    public class Entities
+    {
+        [Serializable()]
+        public struct Tab
+        {
+            public String Name;
+            public String Identifier;
+            public List<Group> Groups; 
+        }
+        [Serializable()]
+        public struct Group
+        {
+            public String Name;
+            public String Identifier;
+            public List<Setting> Settings;
+        }
+        [Serializable()]
+        public struct Setting
+        {
+            public String Name;
+            public String Identifier;
+            public Object Value;
+            public Type Type;
+        }
+    }
+}
