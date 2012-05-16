d89e138bebd0297135a509190695a8e97f357474
Erwin de Haan <git@erayan.eu>
2012-05-16 18:56:52 +0000

    Added update uploader app. Added about window (TODO Update check.)
    git-svn-id: http://192.168.178.2:1337/svn/erayan@113 12523cd5-8ef4-534a-86f2-1c811af9c56e

--- emp/ExceptionHandler/ExceptionHandler.cs
+++ emp/ExceptionHandler/ExceptionHandler.cs
@@ -6,36 +6,36 @@ using System.Windows;
 
 namespace EMP
 {
-    public class ExceptionHandler
-    {
-        /// <summary>
-        ///    The different levels of exceptions
-        /// </summary>
-        public enum ExceptionLevel
-        {
-            FatalError,
-            Error,
-            Warning,
-            Notice            
-        }
-        /// <summary>
-        ///    Handles exceptions
-        /// </summary>
-        /// <param name="message">
-        ///    The error message to display to the user or add to the log.
-        /// </param>
-        /// <param name="eLevel">
-        ///    The severity of the error, a <see cref="ExeptionLevel"/> value. Deafult is <see cref="ExceptionLevel.Notice"/>.
-        /// </param>
-        /// <param name="exception">
-        ///    The <see cref="Exception"/> object returned by the runtime or usercode.
-        /// </param>
-        /// <seealso cref="ExeptionLevel"/>
-        static public void TriggerException(String message, ExceptionLevel eLevel = ExceptionLevel.Notice, Exception exception = null)
-        {
-            //MessageBox.Show(message+"\r\n"+eLevel.ToString());      obsolete
-            //include logging instead
-            
-        }
-    }
+	public class ExceptionHandler
+	{
+		/// <summary>
+		///    The different levels of exceptions
+		/// </summary>
+		public enum ExceptionLevel
+		{
+			FatalError,
+			Error,
+			Warning,
+			Notice
+		}
+		/// <summary>
+		///    Handles exceptions
+		/// </summary>
+		/// <param name="message">
+		///    The error message to display to the user or add to the log.
+		/// </param>
+		/// <param name="eLevel">
+		///    The severity of the error, a <see cref="ExeptionLevel"/> value. Deafult is <see cref="ExceptionLevel.Notice"/>.
+		/// </param>
+		/// <param name="exception">
+		///    The <see cref="Exception"/> object returned by the runtime or usercode.
+		/// </param>
+		/// <seealso cref="ExeptionLevel"/>
+		static public void TriggerException(String message, ExceptionLevel eLevel = ExceptionLevel.Notice, Exception exception = null)
+		{
+			//MessageBox.Show(message+"\r\n"+eLevel.ToString());      obsolete
+			//include logging instead
+
+		}
+	}
 }
--- emp/iTunesCOM/iTunesCOM.cs
+++ emp/iTunesCOM/iTunesCOM.cs
@@ -7,75 +7,76 @@ using iTunesLib;
 
 namespace EMP
 {
-    public class iTunesCOM : IDisposable
-    {
-        iTunesApp iT;        
-        IITLibraryPlaylist LP;        
-        IITPlaylistCollection PC;
-        public event _IiTunesEvents_OnQuittingEventEventHandler iTunesQuit;
-        public event _IiTunesEvents_OnAboutToPromptUserToQuitEventEventHandler iTunesAboutToPromptUser;
-        public String PlayerState
-        {
-            get { return iT.PlayerState.ToString(); }
-        }
-        public IITTrackCollection Tracks
-        {
-            get { return LP.Tracks; }
-        }
-        public IITPlaylistCollection Playlists
-        {
-            get { return PC; }
-        }             
-        public IITPlaylist MoviePlaylist
-        {
-            get { return PC[3]; }
-        }
-        public IITPlaylist TVShowPlaylist
-        {
-            get { return PC[4]; }
-        }
-        public iTunesCOM()
-        {
-            iT = new iTunesApp();
-            foreach (IITWindow Window in iT.Windows)
-            {
-                Window.Minimized = true;
-            }
-            LP = iT.LibraryPlaylist;
-            PC = iT.LibrarySource.Playlists;
-            iT.OnQuittingEvent += new _IiTunesEvents_OnQuittingEventEventHandler(iT_OnQuittingEvent);
-            iT.OnAboutToPromptUserToQuitEvent += new _IiTunesEvents_OnAboutToPromptUserToQuitEventEventHandler(iT_OnAboutToPromptUserToQuitEvent);
-        }
-        public void Dispose(){
-            LP = null;
-            PC = null;
-            iT.OnQuittingEvent -= iT_OnQuittingEvent;
-            iT.OnAboutToPromptUserToQuitEvent -= iT_OnAboutToPromptUserToQuitEvent;
-            iT = null;
-        }
-        protected virtual void OniTunesQuit()
-        {
-            _IiTunesEvents_OnQuittingEventEventHandler handler = iTunesQuit;
-            if (handler != null)
-            {
-                handler();
-            }
-        }
-        protected virtual void OniTunesAboutToPromptUser()
-        {
-            _IiTunesEvents_OnAboutToPromptUserToQuitEventEventHandler handler = iTunesAboutToPromptUser;
-            if (handler != null)
-            {
-                handler();
-            }
-        }
-        private void iT_OnQuittingEvent()
-        {
-            OniTunesQuit();
-        }
-        private void iT_OnAboutToPromptUserToQuitEvent()
-        {
-            OniTunesAboutToPromptUser();
-        }
-    }
+	public class iTunesCOM : IDisposable
+	{
+		iTunesApp iT;
+		IITLibraryPlaylist LP;
+		IITPlaylistCollection PC;
+		public event _IiTunesEvents_OnQuittingEventEventHandler iTunesQuit;
+		public event _IiTunesEvents_OnAboutToPromptUserToQuitEventEventHandler iTunesAboutToPromptUser;
+		public String PlayerState
+		{
+			get { return iT.PlayerState.ToString(); }
+		}
+		public IITTrackCollection Tracks
+		{
+			get { return LP.Tracks; }
+		}
+		public IITPlaylistCollection Playlists
+		{
+			get { return PC; }
+		}
+		public IITPlaylist MoviePlaylist
+		{
+			get { return PC[3]; }
+		}
+		public IITPlaylist TVShowPlaylist
+		{
+			get { return PC[4]; }
+		}
+		public iTunesCOM()
+		{
+			iT = new iTunesApp();
+			foreach (IITWindow Window in iT.Windows)
+			{
+				Window.Minimized = true;
+			}
+			LP = iT.LibraryPlaylist;
+			PC = iT.LibrarySource.Playlists;
+			iT.OnQuittingEvent += new _IiTunesEvents_OnQuittingEventEventHandler(iT_OnQuittingEvent);
+			iT.OnAboutToPromptUserToQuitEvent += new _IiTunesEvents_OnAboutToPromptUserToQuitEventEventHandler(iT_OnAboutToPromptUserToQuitEvent);
+		}
+		public void Dispose()
+		{
+			LP = null;
+			PC = null;
+			iT.OnQuittingEvent -= iT_OnQuittingEvent;
+			iT.OnAboutToPromptUserToQuitEvent -= iT_OnAboutToPromptUserToQuitEvent;
+			iT = null;
+		}
+		protected virtual void OniTunesQuit()
+		{
+			_IiTunesEvents_OnQuittingEventEventHandler handler = iTunesQuit;
+			if (handler != null)
+			{
+				handler();
+			}
+		}
+		protected virtual void OniTunesAboutToPromptUser()
+		{
+			_IiTunesEvents_OnAboutToPromptUserToQuitEventEventHandler handler = iTunesAboutToPromptUser;
+			if (handler != null)
+			{
+				handler();
+			}
+		}
+		private void iT_OnQuittingEvent()
+		{
+			OniTunesQuit();
+		}
+		private void iT_OnAboutToPromptUserToQuitEvent()
+		{
+			OniTunesAboutToPromptUser();
+		}
+	}
 }
