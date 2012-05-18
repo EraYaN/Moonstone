fa677b6a3cdba15ea083c1eefa4ebdb7168c9b89
Erwin de Haan <git@erayan.eu>
2012-05-18 14:12:13 +0000

    Moved library.xsd
    git-svn-id: http://192.168.178.2:1337/svn/erayan@152 12523cd5-8ef4-534a-86f2-1c811af9c56e

---
+++ emp/MediaHandlers/LibraryHelpers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EMP
+{
+	public static class LibraryHelpers
+	{
+		public static Boolean AddMovieToLibrary(Library library, String filePath)
+		{
+			return false;
+		}
+		public static Boolean AddTVShowToLibrary(Library library, String filePath)
+		{
+			return false;
+		}
+	}
+}
