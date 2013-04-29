6b51b6bb57014da37ff00c7eadcb467dd6dbe77c
Robin Hes <ninjatuna@outlook.com>
2013-04-29 17:14:50 +0200

    Added project for local player app

---
+++ win32/src/TestAppLocalPLayer/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestAppLocalPLayer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        PathWindow pathWindow = new PathWindow();
+        public static string musicPath;
+
+        public MainWindow()
+        {
+            InitializeComponent();          
+            pathWindow.PathSet += pathWindow_PathSet;  
+        }
+
+        #region Button handlers
+        private void setPathButton_Click(object sender, RoutedEventArgs e)
+        {
+            pathWindow.Show();
+        }
+
+        private void playpauseButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void nextButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void prevButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+        #endregion
+
+        void pathWindow_PathSet(object sender, EventArgs e)
+        {
+            
+        }
+    }
+
+    public class MusicList
+    {
+
+    }
+}
---
+++ win32/src/TestAppLocalPLayer/PathWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TestAppLocalPLayer
+{
+    public delegate void PathSetEventHandler(object sender, EventArgs e);
+
+    /// <summary>
+    /// Interaction logic for PathWindow.xaml
+    /// </summary>
+    public partial class PathWindow : Window
+    {
+        public event PathSetEventHandler PathSet;
+
+        public PathWindow()
+        {
+            InitializeComponent();
+        }
+
+        protected virtual void OnPathSet(EventArgs e)
+        {
+            if (PathSet != null)
+                PathSet(this, e);
+        }
+
+        private void setButton_Click (object sender, RoutedEventArgs e)
+        {
+            string tmp;
+            tmp = pathTextBox.Text.Trim();
+
+            if (String.IsNullOrEmpty(tmp))
+            {
+                MessageBox.Show("Please enter a valid path.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            else
+            {
+                MainWindow.musicPath = tmp;
+                MessageBox.Show("Music path set to " + MainWindow.musicPath);
+                OnPathSet(EventArgs.Empty);
+                this.Hide();
+            }
+        }
+
+        private void cancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+        }
+    }
+}
