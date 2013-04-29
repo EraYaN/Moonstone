c307dccc98d63cbe2cd9f7781f1cffaa427408e8
Robin Hes <ninjatuna@outlook.com>
2013-04-29 22:28:57 +0200

    File listing done
    Next up: playback

---
+++ win32/src/TestAppLocalPLayer/MusicList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAppLocalPLayer
+{
+    public class MusicList
+    {
+        #region properties
+
+        private string[] filePaths;
+        public string[] FilePaths
+        {
+            get
+            {
+                return filePaths;
+            }
+        }
+
+        #endregion
+
+        public MusicList()
+        {
+            filePaths = Directory.GetFiles(MainWindow.musicPath, "*.mp3",SearchOption.AllDirectories);
+        }
+    }
+}
