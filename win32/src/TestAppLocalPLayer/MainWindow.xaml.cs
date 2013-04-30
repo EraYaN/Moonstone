5148465547ae7ccf2b5785577093032bb18326df
Robin Hes <ninjatuna@outlook.com>
2013-04-30 19:30:28 +0200

    Play/Pause/Resume/Stop tracks

--- win32/src/TestAppLocalPLayer/MainWindow.xaml.cs
+++ win32/src/TestAppLocalPLayer/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -22,6 +23,7 @@ namespace TestAppLocalPLayer
     {
         PathWindow pathWindow = new PathWindow();
         public static string musicPath;
+        Player player;
 
         public MainWindow()
         {
@@ -38,7 +40,37 @@ namespace TestAppLocalPLayer
 
         private void playpauseButton_Click(object sender, RoutedEventArgs e)
         {
+            if (player == null)
+            {
+                if (musiclistListView.SelectedValue != null)
+                {
+                    player = new Player();
+                    player.WaveOutDevice.PlaybackStopped += player_WaveOutDevice_PlaybackStopped;
+                }
+                else
+                {
+                    return;
+                }
+            }
 
+            if (player.PlaybackState == NAudio.Wave.PlaybackState.Paused)
+            {
+                player.Resume();
+                playpauseButton.Content = "Pause";
+                playbackstatusLabel.Content = "Playback resumed";
+            }
+            else if (player.PlaybackState == NAudio.Wave.PlaybackState.Playing)
+            {
+                player.Pause();
+                playpauseButton.Content = "Play";
+                playbackstatusLabel.Content = "Playback paused";
+            }
+            else if (player.PlaybackState == NAudio.Wave.PlaybackState.Stopped)
+            {
+                player.Play((string)musiclistListView.SelectedValue);
+                playpauseButton.Content = "Pause";
+                playbackstatusLabel.Content = "Playback started";
+            }
         }
 
         private void nextButton_Click(object sender, RoutedEventArgs e)
@@ -49,6 +81,28 @@ namespace TestAppLocalPLayer
         private void prevButton_Click(object sender, RoutedEventArgs e)
         {
             
+        }
+
+        private void stopButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (player.PlaybackState != NAudio.Wave.PlaybackState.Stopped)
+            {
+                player.Reset();
+                playbackstatusLabel.Content = "Playback stopped";
+            }
+        }
+
+        private void listViewItem_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (player.PlaybackState != NAudio.Wave.PlaybackState.Stopped)
+            {
+                player.Reset();
+            }
+
+            ListViewItem item = sender as ListViewItem;
+            object obj = item.Content;
+
+            player.Play((string)obj);
         }
         #endregion
 
@@ -63,12 +117,25 @@ namespace TestAppLocalPLayer
             entriesfoundLabel.Content = musicList.FilePaths.Count() + " entries found";
         }
 
+        private void player_WaveOutDevice_PlaybackStopped(object sender, EventArgs e)
+        {
+            if (player != null)
+            {
+                player.Reset();
+            }
+            playpauseButton.Content = "Play";
+            playbackstatusLabel.Content = "Playback stopped";
+        }
+
         private void MainWindow_Closed(object sender, EventArgs e)
         {
             pathWindow.Close();
+
+            if (player != null)
+            {
+                player.Dispose();
+            }
         }
         #endregion
     }
-
-
 }
