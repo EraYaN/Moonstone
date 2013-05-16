45c109f4aa5971282fa6fc43f7c82eacc233afe2
Erwin de Haan <git@erayan.eu>
2013-05-16 12:56:33 +0200

    Created BaseProvider And provider classes.

--- win32/src/Moonstone.Providers/IProvider.cs
+++ win32/src/Moonstone.Providers/IProvider.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace Moonstone.Providers
 {
-    public interface IProvider : IDisposable
+    public interface IProvider : IDisposable, INotifyPropertyChanged
     {
 		
     }
